1d7616bc673d031a406ca31de975cacad5c2942d
osbeg <osbeg@LAPTOP-4T5S3J9P>
2021-10-06 11:38:45 +0100

    Kata2 versión 3

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,16 +1,14 @@
 package kata2;
 
-import java.util.HashMap;
+
 import java.util.Map;
 
 public class Kata2 {
     public static void main(String[] args) {
         int[] data = {1,2,3,5,6,1,3,2,2,3,4,5,1,3,6};
-        Map<Integer,Integer> histogram = new HashMap<>();
-        for (int i : data) {
-            histogram.put(i,histogram.containsKey(i) ? histogram.get(i)+1 : 1);
-        }
-        for (Map.Entry<Integer,Integer> entry : histogram.entrySet()) {
+        Histogram histo = new Histogram(data);
+        Map<Integer,Integer> histogr = histo.getHistogram();
+        for (Map.Entry<Integer,Integer> entry : histogr.entrySet()) {
             System.out.println(entry.getKey()+ "==>" + entry.getValue());
         }
     }
